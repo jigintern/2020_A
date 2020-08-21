eee41e6e3896bb5ad47bc31a425babc69cc97292
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2020-08-21 16:29:22 +0900

    add: 課題を解くのに制限時間を設定

--- static/util.js
+++ static/util.js
@@ -1,4 +1,6 @@
 export function getUuid() {
+    // keyを指定して取得
+    // 「 key1=val1; key2=val2; key3=val3; ・・・ 」というCookie情報が保存されているとする
     var arr = new Array();
     if (document.cookie != '') {
         var tmp = document.cookie.split('; ');
